8ef60430df1ef6051e470c92ddf5361aaf7d57d7
JaeHoon <wogns8030@naver.com>
2024-02-18 18:34:00 +0900

    feat : Category

---
+++ src/main/java/com/example/boardserver/dto/CategoryDTO.java
@@ -0,0 +1,26 @@
+package com.example.boardserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryDTO {
+
+    public enum SortStatus{
+        CATEGORIES, NEWEST, OLDEST
+    }
+
+    private int id;
+    private String name;
+    private SortStatus status;
+    private int searchCount;
+    private int pagingStartOffset;
+
+}
---
+++ src/main/java/com/example/boardserver/mapper/CategoryMapper.java
@@ -0,0 +1,15 @@
+package com.example.boardserver.mapper;
+
+import com.example.boardserver.dto.CategoryDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CategoryMapper {
+
+    public int register(CategoryDTO categoryDTO);
+
+    public void updateCategory(CategoryDTO categoryDTO);
+
+    public void deleteCategory(int categoryId);
+
+}
