f33d77c30d8620c4cdf656047689059b66335b1d
JaeHoon <wogns8030@naver.com>
2024-02-26 07:54:14 +0900

    fix : @Async

--- src/main/java/com/example/boardserver/BoardservcerApplication.java
+++ src/main/java/com/example/boardserver/BoardservcerApplication.java
@@ -3,10 +3,8 @@ package com.example.boardserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableCaching
-@EnableAsync
 @SpringBootApplication
 public class BoardservcerApplication {
 
