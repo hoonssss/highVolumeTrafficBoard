b5b14ac2566cf105891b5787ea0af7917ee15ba3
JaeHoon <wogns8030@naver.com>
2024-03-01 17:13:56 +0900

    fix : 오타 변경

--- src/main/java/com/example/boardserver/BoardServerApplication.java
+++ src/main/java/com/example/boardserver/BoardServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-public class BoardservcerApplication {
+public class BoardServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoardservcerApplication.class, args);
+		SpringApplication.run(BoardServerApplication.class, args);
 	}
 
 }
